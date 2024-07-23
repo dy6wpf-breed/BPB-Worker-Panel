5e4807f0fecd93026db6ad755d8161ba0926c207
dy6wpf-breed <156272478+dy6wpf-breed@users.noreply.github.com>
2024-07-23 15:37:15 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'f11e4c1b-80c6-46f0-9473-842ea5d7beb9';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
